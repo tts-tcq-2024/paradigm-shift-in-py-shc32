f3f1b83ce45024e2509b8f0108917d5d3436458b
shc32 <shc146388@gmail.com>
2024-07-30 13:09:14 +0530

    Update check_limits.py

--- check_limits.py
+++ check_limits.py
@@ -1,18 +1,26 @@
+def check_measure(measure_name, measure_value, lower_limit, upper_limit):
+    results = {}
+    status = True
+    if measure_value < lower_limit:
+        print(measure_name + " is too low!")
+        status = False
+    elif measure_value > upper_limit:
+        print(measure_name + " is too high!")
+        status = False
+    results["status"] = status
+    return results
 
 def battery_is_ok(temperature, soc, charge_rate):
-  if temperature < 0 or temperature > 45:
-    print('Temperature is out of range!')
-    return False
-  elif soc < 20 or soc > 80:
-    print('State of Charge is out of range!')
-    return False
-  elif charge_rate > 0.8:
-    print('Charge rate is out of range!')
-    return False
+    temp_results = check_measure("Temperature", temperature, 0, 45)
+    soc_results = check_measure("State of Charge", soc, 20, 80)
+    charge_results = check_measure("Charge Rate", charge_rate, 0, 0.8)
+    return temp_results["status"] and soc_results["status"] and charge_results["status"]
 
-  return True
-
-
-if __name__ == '__main__':
-  assert(battery_is_ok(25, 70, 0.7) is True)
-  assert(battery_is_ok(50, 85, 0) is False)
+if __name__ == "__main__":
+    # Test cases
+    assert battery_is_ok(25, 70, 0.7) == True
+    assert battery_is_ok(50, 85, 0.0) == False
+    assert battery_is_ok(-1, 70, 0.7) == False
+    assert battery_is_ok(25, 10, 0.7) == False
+    assert battery_is_ok(25, 70, 0.9) == False
+    print("Some more tests needed")
